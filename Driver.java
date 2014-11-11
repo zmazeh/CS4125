29ee1432957f07bcc7624e39f5fbaa1eeb63440c
zmazeh <zaneb_5@msn.com>
2014-11-11 20:39:57 +0000

    Create Driver.java

---
+++ Driver.java
@@ -0,0 +1,8 @@
+public interface Driver {
+	public void setSpeedAbove(int a);//speed above speed limit (if drives below, use negative)
+	public int getVehicleMax(); //returns max speed car can allow
+	public Vehicle getVehicle(); //returns Vehicle currently driving
+	public void setVehicle(); //sets to random vehicle unless is authority driver
+	public void setVehicle(Vehicle car); // sets to specified car
+	void setPersonality();
+}
