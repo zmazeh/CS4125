df25146425728b12d6544eea47c5417da35e57d9
zmazeh <zaneb_5@msn.com>
2014-11-11 20:42:38 +0000

    Create Lane.java

---
+++ Lane.java
@@ -0,0 +1,28 @@
+
+public class FastCar implements Vehicle{
+	int MaxSpeed = 200;
+	int CurrentPos;
+	
+	public int getDistanceBehind(){
+		//TODO
+		return 0;
+	}
+	public int getDistanceInfront(){
+		//TODO
+		return 0;
+	}
+	public void setPosition(int a){
+		CurrentPos = a;
+	}
+	
+	public int getPosition(){
+		return CurrentPos;
+	}
+	
+	public int getMaxSpeed(){
+		return MaxSpeed;
+	}
+		public void create(){
+		System.out.println("fast car created.");
+	}
+}
