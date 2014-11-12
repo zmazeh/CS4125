57ded1653bc33fa9003ef688c3a77fbf86d1d5a5
zmazeh <zaneb_5@msn.com>
2014-11-12 15:12:16 +0000

    Update Lane.java

--- Lane.java
+++ Lane.java
@@ -1,28 +1,35 @@
+import java.util.ArrayList;
+import java.util.Random;
 
-public class FastCar implements Vehicle{
-	int MaxSpeed = 200;
-	int CurrentPos;
-	
-	public int getDistanceBehind(){
-		//TODO
-		return 0;
+//Lanes can only have one direction
+// They store Cars while they are traveling from their point A to point B
+public class Lane {
+	ArrayList<Vehicle> fill = new ArrayList<Vehicle>();
+	String side;
+	Lane() {
+
 	}
-	public int getDistanceInfront(){
-		//TODO
-		return 0;
+
+	// adds random vehicles to lane of a specified direction
+	Lane(String str, int numOfCars) {
+		Random generator = new Random();
+		if (numOfCars > 4) {
+			System.err.println("Too many cars in lane");
+		} else if (numOfCars < 1) {
+			System.err.println("Invalid input for number of cars in lane: "
+					+ numOfCars);
+		} else {
+			for (int i = 0; i < numOfCars; i++) {
+				int randomInt = generator.nextInt(100);
+				if (randomInt % 2 == 0) {
+					Vehicle a = new SlowCar();
+					fill.add(a);
+				} else {
+					Vehicle b = new FastCar();
+					fill.add(b);
+				}
+			}
+		}
 	}
-	public void setPosition(int a){
-		CurrentPos = a;
-	}
-	
-	public int getPosition(){
-		return CurrentPos;
-	}
-	
-	public int getMaxSpeed(){
-		return MaxSpeed;
-	}
-		public void create(){
-		System.out.println("fast car created.");
-	}
+
 }
