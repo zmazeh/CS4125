1c34049eacfca03a35d4f2263319ee18c4e4fd58
MrMallon <12165565@studentmail.ul.ie>
2014-11-18 11:41:39 +0000

    Update DisplayClass.java

--- DisplayClass.java
+++ DisplayClass.java
@@ -8,6 +8,12 @@ import java.io.File;
 
 public class DisplayClass
 {
+  public static Cars panel;
+  public static KeyBindings input2;
+
+  public DisplayClass(){
+  }
+
   public static void main(String[]agrs)throws java.io.IOException
   {
   BufferedImage blueCar = ImageIO.read(new File("blueCar.png"));
@@ -16,22 +22,18 @@ public class DisplayClass
   BufferedImage redCar = ImageIO.read(new File("redCar.png"));
   BufferedImage redCar2 = ImageIO.read(new File("redCar2.png"));
 
-  BufferedImage pause = ImageIO.read(new File("pause.png"));
-  BufferedImage start = ImageIO.read(new File("play.png"));
+  BufferedImage road = ImageIO.read(new File("road.jpg"));
 
   final  JPanel panelRoad = new JPanel();
 
   JFrame frame = new JFrame("CAR SIMULATOR");
   frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-  frame.setSize(700,600);
+  frame.setSize(700,400);
 
   Container pane = frame.getContentPane();
-  Game panel = new Game(blueCar,blueCar2,redCar,redCar2,pause,start);
+  panel = new Cars(blueCar,blueCar2,redCar,redCar2,road);
   pane.add(panel);
 
-
-
-  //frame.setContentPane(new JLabel(new ImageIcon(ImageIO.read(new File("wallhaven.jpg")))));
   frame.setVisible(true);
   }
 }
